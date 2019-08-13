617a6eb77698324646ae50ec26a0379528e1e959
mario <emmariodelar@gmail.com>
2019-08-13 23:19:14 +0200

    implement react

---
+++ single-spa.config.js
@@ -0,0 +1,13 @@
+import {registerApplication, start} from 'single-spa'
+
+registerApplication(
+  // Name of our single-spa application
+  'home',
+  // Our loading function
+  () => import('./src/home/home.app.js'),  // Our activity function
+  () => location.pathname === "" || 
+    location.pathname === "/" ||
+    location.pathname.startsWith('/home')
+);
+
+start()
